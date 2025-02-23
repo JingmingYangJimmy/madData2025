e55bae59d3c1ab08a4c5f69afe9893123821c5f9
tfranke0814 <tysonfranke2004@gmail.com>
2025-02-23 09:43:37 -0600

    add series output TODO: change carousel

--- backend/get_user_preference.py
+++ backend/get_user_preference.py
@@ -14,7 +14,7 @@ def load_json(filepath):
         return json.load(file)  # Use json.load() for normal JSON format
 
 
-def calculate_score(movie, user_ratings):
+def calculate_score(movie, user_ratings): # not currently implemented, see below
     return (
         abs(movie["AggregatedEmotion"]["Sad-Happy"] - user_ratings["happy_index"]) +
         abs(movie["AggregatedEmotion"]
@@ -23,11 +23,19 @@ def calculate_score(movie, user_ratings):
 
 
 if __name__ == "__main__":
-    # LOAD MOVIE DATA WITH EMOTIONAL RATINGS
+    # LOAD MOVIE AND SERIES DATA WITH EMOTIONAL RATINGS
+    df_m = pd.read_json(
+        "../EmotionalMovies.json")[["Title", "Genre", "AggregatedEmotion"]]
     df_s = pd.read_json(
         "../EmotionalMovies.json")[["Title", "Genre", "AggregatedEmotion"]]
 
-    # Generated list of genres
+    # Generated list of genres for movies
+    lstm = []
+    for gm in df_m["Genre"]:
+        for g in gm.split(", "):
+            if g not in lstm:
+                lstm.append(g)
+    # Generated list of genres for series
     lsts = []
     for gs in df_s["Genre"]:
         for g in gs.split(", "):
@@ -35,6 +43,20 @@ if __name__ == "__main__":
                 lsts.append(g)
 
     # Change movie data with emotional ratings to desired format
+    df_m["SadHappy"] = df_m["AggregatedEmotion"].apply(
+        lambda x: x["Sad-Happy"])
+    df_m["TenseCalm"] = df_m["AggregatedEmotion"].apply(
+        lambda x: x["Tense-Calm"])
+    df_m['genre_indices'] = df_m['Genre'].apply(
+        lambda x: [lstm.index(g) for g in x.split(", ") if g in lstm])
+    df_m['genre_row'] = df_m['genre_indices'].apply(
+        lambda x: [1 if i in x else 0 for i in range(len(lstm))])
+    df_m["matrixrow"] = df_m.apply(
+        lambda row: [row["SadHappy"], row["TenseCalm"]] + row["genre_row"], axis=1)
+    df_m[["Title", "matrixrow"]]
+    X_m = np.array(df_m["matrixrow"].tolist())
+
+    # Change series data with emotional ratings to desired format
     df_s["SadHappy"] = df_s["AggregatedEmotion"].apply(
         lambda x: x["Sad-Happy"])
     df_s["TenseCalm"] = df_s["AggregatedEmotion"].apply(
@@ -46,7 +68,7 @@ if __name__ == "__main__":
     df_s["matrixrow"] = df_s.apply(
         lambda row: [row["SadHappy"], row["TenseCalm"]] + row["genre_row"], axis=1)
     df_s[["Title", "matrixrow"]]
-    X = np.array(df_s["matrixrow"].tolist())
+    X_s = np.array(df_s["matrixrow"].tolist())
 
     # LOAD USER DATA
     backend_data = load_json('data.json')  # Normal JSON for backend data
@@ -61,18 +83,29 @@ if __name__ == "__main__":
     user_ratings = np.array([latest_bar_values['happy_index'],
                              latest_bar_values['calm_index']] + genre_row)
 
-    array = X@user_ratings
+    # Get rankings based on user_ratings for movies and series
+    array_m = X_m@user_ratings
+    array_s = X_s@user_ratings
 
-    res_df_s = pd.DataFrame(array, columns=["Value"])
+    res_df_m = pd.DataFrame(array_m, columns=["Value"])
+    res_df_m["Title"] = df_m["Title"]
+    res_df_m_sorted = res_df_m.sort_values(by="Value", ascending=False)
+
+    res_df_s = pd.DataFrame(array_s, columns=["Value"])
     res_df_s["Title"] = df_s["Title"]
     res_df_s_sorted = res_df_s.sort_values(by="Value", ascending=False)
 
-    # LOAD ALLL MOVIES DATA FROM movies_data.json
+    # LOAD ALLL MOVIES DATA FROM movies_data.json and ALL SERIES DATA FROM tv_series_data.json
     movies_data = load_json("movies_data.json")
     movies_df = pd.DataFrame(movies_data)
+    series_data = load_json("tv_series_data.json")
+    series_df = pd.DataFrame(series_data)
 
     # Merge by title and output to frontend
-    merged_df = res_df_s_sorted.merge(movies_df, on="Title", how="left")
+    merged_df_m = res_df_s_sorted.merge(movies_df, on="Title", how="left")
+    merged_df_s = res_df_m_sorted.merge(series_df, on="Title", how="left")
     desired_columns = ["Title", "Poster", "imdbID", "Year", "Plot"]
-    filtered_df = merged_df[desired_columns]
-    print(filtered_df.head(100).to_json(orient="records", indent=4))
+    filtered_df_m = merged_df_m[desired_columns]
+    filtered_df_s = merged_df_s[desired_columns]
+    print(filtered_df_m.head(100).to_json(orient="records", indent=4))
+    print(filtered_df_s.head(100).to_json(orient="records", indent=4))
